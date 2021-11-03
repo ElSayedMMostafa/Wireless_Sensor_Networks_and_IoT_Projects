04965bc8355a0894500df96b13f6dafcc5ffc5eb
ElsayedMMostafa <s-ayedmmostafa@zewailcity.edu.eg>
2021-11-03 14:51:23 +0200

    modified the main script

--- Project 1 | WSN Generation and Life Time Calculation/main_script.m
+++ Project 1 | WSN Generation and Life Time Calculation/main_script.m
@@ -1,5 +1,5 @@
 clear; clc; close all;
-rng(25); %set the random seed!
+rng(24); %set the random seed!
 
 %% WSN Parameters
 N = 100; %The number of sensors
@@ -10,8 +10,9 @@ d0 = sqrt(eta_short/eta_long);
 E_initial = 2e9; % in nJ (the starting energy)
 E_elec = 50; E_agg = 50;
 k = 625*1024; %number_of_bits per cycle
-func_tx_energy = @(E,eta,exp,d) max(0, E - (k*(E_elec+E_agg) + k*eta*d^exp));
-func_rx_energy = @(E) max(0, E - k*(E_elec+E_agg));
+func_tx_energy = @(E,eta,exp,d) max(0, E - (k*(E_elec + E_agg) + k*eta*d^exp));
+func_rx_energy = @(E) max(0, E - (k*(E_elec+E_agg)));
+
 %% Generating the location of senssors
 locs = cat(1, randperm(area_width), randperm(area_height))';
 % assert no node existance @ the network center
@@ -32,8 +33,10 @@ dists = sqrt(sum((locs-[50 50]).^2,2));
 %% Initiate the energy
 energy = E_initial*ones(1,N+1);
 %% GO on cycles!
-dead_nodes =[];
-while ~ all(energy == 0)
+active_nodes =[];
+
+while 1
+    % Calculate the dissipated & remaining energy
 for i=1:N
     dist = dists(i);
     if dist <= d0
@@ -43,9 +46,20 @@ for i=1:N
     end
 end
 energy(N+1) = func_rx_energy(energy(N+1));
-dead_nodes = cat(2, dead_nodes, sum(energy<=k*(E_elec+E_agg)));
+    % Get the no. of active nodes
+    rx_threshold = k*(E_elec+E_agg);
+    tx_threshold = rx_threshold + k*eta_short*0.5^2;
+active_nodes = cat(2, active_nodes, sum(energy(1:N) >= tx_threshold));
+
+if active_nodes(end) == 0 || energy(N+1) < rx_threshold
+    break;
+end
 end
     
 figure('Name', 'Curve of Active Nodes')
-plot(dead_nodes);
+plot(linspace(1,length(active_nodes),length(active_nodes)), active_nodes);
+grid on
+xlabel('no. of cycle');
+ylabel('no. of active nodes');
+title('The number of active nodes at each cycle');
 
