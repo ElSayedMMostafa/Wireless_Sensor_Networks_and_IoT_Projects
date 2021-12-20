09306ee0842c5585d8483e674f10a4580980abe1
ElsayedMMostafa <s-ayedmmostafa@zewailcity.edu.eg>
2021-12-20 07:55:15 +0200

    alternative criteria devolped

--- Project 2 - WSN Generation with dual hop transmission/alternative_intermediate_selection.m
+++ Project 2 - WSN Generation with dual hop transmission/alternative_intermediate_selection.m
@@ -3,13 +3,17 @@ function [max_Index, dist_to_node] = alternative_intermediate_selection(node_ind
 
 % This function selects the best intermediate node for dual hop
 % transmission given a specific transmitter node assuming that the sink in
-% located in [50, 50]
+% located in [50, 225]
+
+% The choice of the intermediate node here is only dependant on the
+% remaining enrgies for the possible intermediate nodes.
+
 % @ Inputs:
     % node_index: The index of tx node location
     % locations: locations of the all nodes (except for the sink)
     % dists_to_sink: 1D vector contains distances from nodes to sink
 % @ Outputs:
-    % min_Index: The index of the chosen node
+    % max_Index: The index of the chosen node
     % dist_to_node: The distance between the tx node and its intermediate
 node = locations(node_index, :);
 locations(node_index, :) = [inf, inf];
@@ -17,11 +21,8 @@ remaining_enrgies(node_index) = -inf;
 
 
 dists_to_node = sqrt(sum((locations-node).^2,2));
-%dists_sum = dists_to_sink + dists_to_node;
 
 
 [~, max_Index] = max(remaining_enrgies); 
 dist_to_node = dists_to_node(max_Index);
-%dist_to_node = dists_to_node(min_Index);
-
 end
